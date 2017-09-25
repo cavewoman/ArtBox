8b58301f8ba6ffc112f1f70d6c6f34229a0540bb
cavewoman <anna.chan365@gmail.com>
2017-09-25 18:11:20 -0400

    #151407614 [Anna] Adds color coordination for supplies

--- ArtBox/SuppliesViewController.swift
+++ ArtBox/SuppliesViewController.swift
@@ -11,6 +11,13 @@ import UIKit
 class SuppliesViewController: UITableViewController {
   var supplyStore: SupplyStore!
   
+  enum SupplyAmount {
+    case moreThanHalfFull
+    case halfFull
+    case lessThanHalfFull
+    case empty
+  }
+  
   required init?(coder aDecoder: NSCoder) {
     super.init(coder: aDecoder)
     navigationItem.leftBarButtonItem = editButtonItem
@@ -31,6 +38,7 @@ class SuppliesViewController: UITableViewController {
     
     cell.textLabel?.text = supply.name
     cell.detailTextLabel?.text = "\(supply.amount ?? 0.0)"
+    setBackgoundColor(cell: cell, amount: supply.amount!)
     
     return cell
   }
@@ -65,4 +73,69 @@ class SuppliesViewController: UITableViewController {
     
   }
   
+  func setBackgoundColor(cell: UITableViewCell, amount: Double){
+    switch getAmountType(amount: amount) {
+    case .moreThanHalfFull:
+      cell.backgroundColor = UIColor().colorFromHex(hexValue: "35226a")
+    case .halfFull:
+      cell.backgroundColor = UIColor().colorFromHex(hexValue: "613dc1")
+    case .lessThanHalfFull:
+      cell.backgroundColor = UIColor().colorFromHex(hexValue: "c7c9f2")
+    default:
+      cell.backgroundColor = UIColor().colorFromHex(hexValue: "ffffff")
+      
+    }
+  }
+  
+  func getAmountType(amount: Double?) -> SupplyAmount {
+    if let amount = amount {
+      if amount > 0.5 {
+        return .moreThanHalfFull
+      } else if amount == 0.5 {
+        return .halfFull
+      } else if (amount > 0) && (amount < 0.5) {
+        return .lessThanHalfFull
+      } else {
+        return .empty
+      }
+    } else {
+      return .empty
+    }
+  }
+  
+}
+
+
+extension UIColor {
+  func colorFromHex(hexValue: String) -> UIColor {
+    let (redText, greenText, blueText) = parseHexString(hexValue: hexValue)
+    
+    let redFloat = Float(redText)
+    let greenFloat = Float(greenText)
+    let blueFloat = Float(blueText)
+    
+    let red: Float = redFloat! / 0xff
+    let green: Float = greenFloat! / 0xff
+    let blue: Float = blueFloat! / 0xff
+    
+    return UIColor(colorLiteralRed: red, green: green, blue: blue, alpha: 1)
+  }
+  
+  func parseHexString(hexValue: String) -> (String, String, String) {
+    var redText: String = "0x"
+    var greenText: String = "0x"
+    var blueText: String = "0x"
+    
+    for (index, char) in hexValue.characters.enumerated() {
+      switch index {
+      case 0, 1:
+        redText += "\(char)"
+      case 2, 3:
+        greenText += "\(char)"
+      default:
+        blueText += "\(char)"
+      }
+    }
+    return (redText, greenText, blueText)
+  }
 }
